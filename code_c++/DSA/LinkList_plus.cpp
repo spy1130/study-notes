e825510bc34ba29a2285b00a95cf5f361039a2cb
spric <734735293@qq.com>
2023-04-11 17:51:52 +0800

    ×öÍêÁË˫ÏòÁ´±í£¬·¢ÏÖһ¿ªʼµÄÇé¿öҲµÿ¼Âǣ¬printº¯ÊýҲµôòӡÍê

--- code_c++/DSA/LinkList_plus.cpp
+++ code_c++/DSA/LinkList_plus.cpp
@@ -10,58 +10,77 @@ typedef struct Data
     string name;
     int age;
 } Data;
-typedef struct Lnode
+typedef struct DuLnode
 {
-    Data note;   // 结点的数据域
-    Lnode *next; // 结点的指针域
-} Lnode, *LinkList;
-bool InitList(LinkList &L) // 插入题外话：LinkList &L等价于 Lnode *&L，Lnode *&L是一个指向指针的引用
+    Data note;             // 结点的数据域
+    DuLnode *prior, *next; // 结点的指针域
+} DuLnode, *LinkList;
+bool InitList(LinkList &L) // 插入题外话：LinkList &L等价于 DuLnode *&L，DuLnode *&L是一个指向指针的引用
 {
-    L = new Lnode;     // 堆区开辟一个头结点，结点的数据类型为Lnode
-    L->next = nullptr; // 空表，也就是说头结点的指针指向为空
+    L = new DuLnode;    // 堆区开辟一个头结点，结点的数据类型为DuLnode
+    L->prior = nullptr; // 双向链表
+    L->next = nullptr;  // 空表，也就是说头结点的指针指向为空
     return true;
 }
 void CreatListHead(LinkList &L, const int n)
 {
     for (int i = 0; i < n; ++i)
     {
-        Lnode *p = new Lnode;
+        DuLnode *p = new DuLnode;
+        if (p == NULL)
+        {
+            cout << "Memory allocation failed." << endl;
+            return;
+        }
         p->note.name = rand_str(5);
         // cin >> "input name:">>p->note.name;
         p->note.age = rand() % 100;
         // cin >> "input age:">>p->note.age;
-        p->next = L->next;
+        p->prior = L;
+        if (L->next == NULL)
+        { // the list is empty
+            p->next = NULL;
+        }
+        else
+        { // the list is not empty
+            p->next = L->next;
+            L->next->prior = p;
+        }
         L->next = p;
     }
 }
 void CreatListTail(LinkList &L, const int n)
 {
-    Lnode *r = L;
+    DuLnode *r = L; // 尾节点，
     for (int i = 0; i < n; ++i)
     {
-        Lnode *p = new Lnode;
+        DuLnode *p = new DuLnode;
+
+        if (p == NULL)
+            return; // 检查是否分配成功
         p->note.name = rand_str(5);
         // cin >> "input name:">>p->note.name;
         p->note.age = rand() % 100;
         // cin >> "input age:">>p->note.age;
-        p->next = nullptr;
+        p->prior = r;
+        p->next = r->next; // r->next如果不是循环链表则一直为空
         r->next = p;
-        r = p;
+        r = p; // 把r移动到末尾
     }
 }
 void printlist(const LinkList &L, const int len)
 {
-    Lnode *p = L->next;
+    DuLnode *p = L->next;
     for (int i = 1; i <= len; i++)
     {
-        cout << p->note.name << ':'<<p->note.age<<" ";
-        p=p->next;
+        cout << p->note.name << ':' << p->note.age << " ";
+        p = p->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 int GetLength(const LinkList &L)
 {
-    Lnode *p;
+    DuLnode *p;
     int i = 0;
     p = L->next;
     while (p != nullptr)
@@ -78,7 +97,7 @@ bool GetElem(const LinkList &L, const int &i, Data &e)
         cerr << "out of range" << endl;
         return false;
     }
-    Lnode *p = L->next;
+    DuLnode *p = L->next;
     for (int j = 1; j < i; ++j)
     {
         p = p->next;
@@ -93,7 +112,7 @@ bool GetElem(const LinkList &L, const int &i, Data &e)
 }
 int LocateElem(LinkList &L, Data &e)
 {
-    Lnode *p = L->next;
+    DuLnode *p = L->next;
     int cnt = 1;
     while (p)
     {
@@ -107,41 +126,80 @@ int LocateElem(LinkList &L, Data &e)
     cerr << "not found" << endl;
     return cnt;
 }
-bool InsertList(LinkList &L, const int &i, const Data &e)
+bool InsertList(LinkList &L, const int i, const Data &e)
 {
-    Lnode *p = L;
-    int j = 0;                     // 这里可以判断在第一个节点前插入时的情况
-    for (j; (j < i - 1) && p; j++) // 范围1到n+1
-    {                              // p！=NULL
+    // 移动指针到i处
+    DuLnode *p = L->next;
+    int j = 1;
+    while ((p->next) && (j < i))
+    {
+        ++j;
         p = p->next;
     }
-    if (!p || j > i - 1)
+    if (j < i || j < 1) // 如果i在链表范围内，上面的while循环的终止条件就是j<i
+    {
+        cerr << "out of range" << endl;
         return false;
-    LinkList s = new Lnode;
+    }
+    // 在堆区创建要插入的结点
+    DuLnode *s = new DuLnode;
+    if (s == NULL) // 检查内存分配是否成功
+    {
+        cout << "Memory allocation failed." << endl;
+        return false;
+    }
     s->note = e;
-    s->next = p->next; // s指向p的下一个
-    p->next = s;       // p指向s
+    // 重新建立链接关系
+    if (i == 1) // 如果插入位置为第一个节点
+    {
+        s->prior = L;  // s的前趋为头节点
+        s->next = p;   // s的后继为原来的第一个节点
+        if (p != NULL) // 如果链表不为空
+        {
+            p->prior = s; // 原来的第一个节点的前趋为s
+        }
+        L->next = s; // 头节点的后继为s
+    }
+    else // 如果插入位置不是第一个节点
+    {
+        s->prior = p->prior; // s的前趋等于p的前趋
+        p->prior->next = s;  // p的前趋结点的next指向s
+        s->next = p;         // s的后继指向p
+        p->prior = s;        // p的前趋改为指向s
+    }
+    // return
     return true;
 }
-bool deletelist(LinkList &L, const int &i, Data &e)
+bool deletelist(LinkList &L, const int i, Data &e)
 {
-    Lnode *p = L, *s;
-    int j = 0;
-    for (j ; (j < i - 1) && p->next; j++) // 范围1到n
+    // 移动指针到i处
+    DuLnode *p = L->next;
+    int j = 1;
+    while (p->next && j < i)
     {
+        ++j;
         p = p->next;
     }
-    if (!(p->next) || j > i - 1)
+    if (j < i || j < 1) // 如果i在链表范围内，上面的while循环的终止条件就是j<i
+    {
+        cerr << "out of range" << endl;
         return false;
-    s = p->next;
-    p->next = p->next->next;
-    e = s->note; // 保存删除的节点
-    delete s;
+    }
+    // 改变链接关系
+    p->prior->next = p->next; // p的前趋结点的next等于p的后继
+    if ((p->next))            // 如果删除的不是最后一个元素
+    {
+        p->next->prior = p->prior;
+    }
+    e = p->note; // 保存删除的节点
+    // 释放p
+    delete p;
+    // 结束
     return true;
 }
 bool clearlist(LinkList &L)
 {
-    Lnode *p, *q;
+    DuLnode *p, *q;
     p = L->next;
     while (p != nullptr)
     {
@@ -187,18 +245,18 @@ int main()
     S.name = "spric";
     S.age = 18;
     InitList(L);
-    CreatListTail(L, 4);
-    CreatListHead(L, 5);
+    // CreatListTail(L, 9);
+    CreatListHead(L, 9);
     InsertList(L, 3, S);
     int length = GetLength(L);
     printlist(L, length);
     GetElem(L, 5, e);
-    cout << e.name <<":"<< e.age << endl;
+    cout << e.name << ":" << e.age << endl;
     int locate_nember = LocateElem(L, S);
     cout << locate_nember << endl;
     deletelist(L, 2, e);
     printlist(L, length - 1);
-    cout << e.name << ":"<< e.age << endl;
+    cout << e.name << ":" << e.age << endl;
     clearlist(L);
     IsEmpty(L);
     DestroyList(L);
