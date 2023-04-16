2059679a5ce80494a1f547a0290e02416fb2cdc7
spric <734735293@qq.com>
2023-04-16 12:03:34 +0800

    链栈和线性栈的实现

---
+++ code_c++/DSA/Linear Stack.cpp
@@ -0,0 +1,103 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#define ElemType int
+#define MAXSIZE 50
+using namespace std;
+
+typedef struct SqStack
+{
+    ElemType *base;//底部
+    ElemType *top;//顶部的上一格，方便操作（子弹匣状）
+    int stacksize;//maxsize
+}SqStack;
+
+bool InitStack(SqStack &S)
+{
+    S.base = new ElemType[MAXSIZE];//ElwmTypr类型的数组用Maxsize这么多的单元空间
+    if (!S.base)
+    {
+        cerr << "failed to get memory" << endl;
+        return false;
+    }
+    S.top = S.base;//空栈标志
+    S.stacksize = MAXSIZE; 
+    return true;
+}
+bool Push(SqStack &S, ElemType &e)
+{
+    //判断栈是否已满
+    if((S.top-S.base) == S.stacksize)//指针相减为地址数相减
+    {
+        cerr << "full of stack" << endl;
+        return false;
+    }
+    *(S.top) = e;
+    ++(S.top);
+    return true;
+}
+bool CreatStack(SqStack &S, const int n)
+{
+    for (int i = 0; i < n;++i)
+    {
+        ElemType input;
+        cin >> input;
+        if(!Push(S,input))
+        {
+            cerr << "error happend at-" << i << endl;
+            return false;
+        }
+    }
+    return true;
+}
+bool IsEmpty(SqStack &S)
+{
+    if (S.base == S.top)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool Pop(SqStack &S, ElemType &e)//弹栈
+{
+    if(!IsEmpty(S))
+    {
+        cerr << "empty stack!error" << endl;
+        return false;
+    }
+    --(S.top);
+    e = *(S.top);
+    return true;
+}
+int StackLength(SqStack &S)
+{
+    return static_cast<int>(S.top - S.base);
+}
+bool ClearStack(SqStack &S)
+{
+    if(S.base)
+    {
+        S.top = S.base;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+void DestoyStack(SqStack &S)
+{
+    if(S.base)
+    {
+        delete[] S.base;
+        S.stacksize = 0;
+        S.base = S.top = nullptr;
+    }
+}
+int main(){
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
---
+++ code_c++/DSA/Link Stack.cpp
@@ -0,0 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+typedef struct StackNode
+{
+    int data;
+    StackNode *next;
+} * LinkStack;
+void InitStack(LinkStack &S)
+{
+    //创建头结点
+    S = new StackNode;
+    S->next = nullptr;
+    S->data = 0; //表示栈的元素个数
+}
+//王老师的视频中没有设置头结点
+//我设置了一个头结点，在头结点的数据域中保存栈的元素个数
+
+void Push(LinkStack &S, const int &e)
+{
+    //插入元素
+    StackNode *temp = new StackNode;
+    temp->data = e;
+    temp->next = S->next;
+    S->next = temp;
+    ++(S->data);//元素个数加一
+}
+void Pop(LinkStack &S, int &e)
+{
+    //删除元素
+    StackNode *p = S->next;
+    e = p->data;
+    S->next = p->next;
+    --(S->data);
+    delete p;
+}
+void CreatStack(LinkStack &S, const int n)
+{
+    int input;
+    for (int i = 0; i < n;++i)
+    {
+        cin >> input;
+        Push(S, input);
+    }
+}
+int StackLength(LinkStack &S)
+{
+    return S->data;
+}
+bool IsEmpty(LinkStack &S)
+{
+    if(!(S->next))
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+bool ClearStack(LinkStack &S)
+{
+    if(!(S->next))
+    {
+        cerr << "empty Stack" << endl;
+        return false;
+    }
+    StackNode *q, *p = S->next;
+    while(p)
+    {
+        q = p;
+        p = p->next;
+        delete q;
+    }
+    S->data = 0;
+    S->next = nullptr;
+    return true;
+}
+bool DestoryStack(LinkStack &S)
+{
+    if(S)
+    {
+        cerr << "error" << endl;
+        return false;
+    }
+    while(S)
+    {
+        StackNode *temp = S;
+        S = S->next;
+        delete temp;
+    }
+    return true;
+}
+int main(){
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
